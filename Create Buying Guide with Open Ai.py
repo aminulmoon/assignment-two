2d8d28d2098c65eac379c13d745a1581284be593
aminulmoon <aminulislammoon@gmail.com>
2022-11-14 17:37:12 +0600

    Generate Introduction

--- Create Buying Guide with Open Ai.py
+++ Create Buying Guide with Open Ai.py
@@ -5,13 +5,31 @@ load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-response = openai.Completion.create(
-  model="text-davinci-002",
-  prompt="Write an introduction in 150 words on “best wireless mouse”",
-  temperature=0.7,
-  max_tokens=256,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0
-)
-print(response)
+def intro_que(key):
+  """
+  This function will make a query to generate intro from OpenAi.
+  """
+  query = f"Write an introduction on “{key}” within 150 words."
+  return str(query)
+
+def query_ans(text):
+  """
+  This function is for generating text/article for any query from OpenAi.
+  :param text: Provide the query variable here.
+  :return: This will return answer for your query.
+  """
+  response = openai.Completion.create(
+    model="text-davinci-002",
+    prompt=text,
+    temperature=0.7,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+  )
+  data = response.get('choices')[0].get('text')
+  return data
+
+intro_query = intro_que('best wireless mouse')
+introduction = query_ans(intro_query)
+print(introduction)
